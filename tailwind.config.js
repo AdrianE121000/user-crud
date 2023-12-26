320f8c9bee79cbcea9b584c4e01f183e1d6b3018
AdrianE121000 <paracosa3s@gmail.com>
2023-12-26 16:06:03 -0500

    config animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slide: {
+          '0%': {
+            transform: 'translateY(-100%)',
+          },
+          '100%': {
+            transform: 'translateY(0)',
+          },
+        },
+      },
+      animation: {
+        slowTop: 'slide 0.6s ease-in-out',
+      },
+    },
   },
   plugins: [],
 };
